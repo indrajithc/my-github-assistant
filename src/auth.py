2968a7022ecf1a767067ecec8152103c0a61e675
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-05-17 22:52:29 +0530

    refactor: login logic caching added

--- src/auth.py
+++ src/auth.py
@@ -2,37 +2,55 @@ from selenium.webdriver.common.by import By
 from src.utils.common_utils import wait_until_element_present
 from src.utils.common_utils import wait_until
 from src.utils.cookies_utils import save_cookies
+from src.utils.cookies_utils import load_cookies
+from src.utils.constants import SESSION_USER_XPATH
+from src.utils.constants import GITHUB_HOME_URL
+import time
 
 
-def check_logged_in(driver): 
+def check_logged_in(driver):
     print("Checking if logged in...")
     try:
-      profile_link =   "//*[@aria-label='Open user account menu']"
-      element = driver.find_element("xpath",profile_link)
-      if element:
-        print("Element found")
-        return True
-      else:
-        raise Exception("Element not found")
+        element = driver.find_element(By.XPATH, SESSION_USER_XPATH)
+        if element:
+            print("Element found")
+            return True
+        else:
+            raise Exception("Element not found")
     except:
-      print("Element not found")
-      return False
+        print("Element not found")
+        return False
+
 
 def login_github(driver, credentials):
     """
     Log in to GitHub using the provided driver and credentials.
     """
-    driver.get('https://github.com')
+    driver.get(GITHUB_HOME_URL)
     
-        
+    print("============================== LOGIN ==============================")
+    time.sleep(2)
+
+    try:
+        load_cookies(driver)
+    except Exception as e:
+        print(e)
+        print("No cookies found")
+
+    time.sleep(1)
+    
+    driver.get(GITHUB_HOME_URL)
+
     is_not_logged_in = driver.find_elements(By.LINK_TEXT, 'Sign in')
     
+    print("is_not_logged_in: ", is_not_logged_in)
+
     if is_not_logged_in:
         sign_in_link = driver.find_element(By.LINK_TEXT, 'Sign in')
         sign_in_link.click()
- 
+
         # Wait until element is present, visible, and interactable
-        wait_until_element_present(driver, (By.NAME, 'login')) 
+        wait_until_element_present(driver, (By.NAME, 'login'))
 
         username_input = driver.find_element(By.NAME, 'login')
         password_input = driver.find_element(By.NAME, 'password')
@@ -40,22 +58,18 @@ def login_github(driver, credentials):
         username_input.send_keys(credentials.username)
         password_input.send_keys(credentials.password)
         password_input.submit()
-        
-        profile_link =  "//*[@aria-label='Open user account menu']"
-        
+
         try:
-          if not check_logged_in(driver):
-            raise Exception("Failed to log in to GitHub.")
-          print("Successfully logged in to GitHub.")
+            if not check_logged_in(driver):
+                raise Exception("Failed to log in to GitHub.")
+            print("Successfully logged in to GitHub.")
         except:
-          def callback_check_logged_in():
-            return check_logged_in(driver)
-          
-          print("Failed to log in to GitHub.")
-          wait_until(callback_check_logged_in, 360)
-        
-        if driver.find_elements("xpath", profile_link):
-          print("Successfully logged in to GitHub.")
-          save_cookies(driver)
-          
-        
+            def callback_check_logged_in():
+                return check_logged_in(driver)
+
+            print("Failed to log in to GitHub.")
+            wait_until(callback_check_logged_in, 360)
+
+    if driver.find_elements(By.XPATH, SESSION_USER_XPATH):
+        print("Successfully logged in to GitHub.")
+        save_cookies(driver)
