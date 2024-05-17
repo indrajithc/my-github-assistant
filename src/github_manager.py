b277cf87dc5420375d1a4a73ad2cb26a98e5c61e
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-05-17 23:07:31 +0530

    feat:  open_user_account_menu added

--- src/github_manager.py
+++ src/github_manager.py
@@ -1,7 +1,28 @@
-import os
-import getpass  
 from src.auth import login_github
 from src.utils.credentials import get_github_credentials
+from src.utils.common_utils import wait_until_element_present
+from src.utils.constants import SESSION_USER_XPATH
+from selenium.webdriver.common.by import By
+from src.utils.common_utils import wait_until
+
+
+
+def open_user_account_menu(driver):
+    """
+    Open user account menu.
+    """
+    print("Opening user account menu...")
+    driver.find_element(By.XPATH, SESSION_USER_XPATH).click()
+    
+    def check_element():
+        if wait_until_element_present(driver, (By.XPATH,SESSION_USER_XPATH)):
+            return True
+        else:
+            return False
+
+    wait_until(check_element, 360)
+            
+    
  
 def main_action(driver):
 
@@ -10,5 +31,10 @@ def main_action(driver):
     
     # Login to GitHub
     login_github(driver, github_credentials)
+    
+    # Open user account menu
+    open_user_account_menu(driver)
+    
+    
 
  
