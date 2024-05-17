d717df94fa72decfe438c210249012d53f1053e3
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-05-17 15:39:28 +0530

    refactor: auto close false, default incognito

--- src/utils/selenium_utils.py
+++ src/utils/selenium_utils.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
-
+from selenium.webdriver.chrome.options import Options
 def get_selenium_driver():
     """
     Get a Selenium driver instance.
     """
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_argument("--incognito")
+
     # Add logic to choose the appropriate driver (e.g., Chrome, Firefox, etc.) here
     # For now, let's use Chrome driver as an example
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=chrome_options)
     return driver
