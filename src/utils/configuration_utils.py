05036818741cb70bdae1e8041d2cabd398c7a907
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-05-18 00:13:52 +0530

    refactor: dynamic path

--- src/utils/configuration_utils.py
+++ src/utils/configuration_utils.py
@@ -9,27 +9,27 @@ DATA_JSON_NAME = "dynamic.json"
 DATA_JSON_PATH = os.path.join(CODE_CONFIGURATION, DATA_JSON_NAME)
 
 
-def read_json_file():
-    cookies = {}
+def read_json_file(path):
+    data = {}
     try:
-        if os.path.exists(DATA_JSON_PATH):
-            with open(DATA_JSON_PATH, 'r') as file:
-                cookies = json.load(file)
+        if os.path.exists(path):
+            with open(path, 'r') as file:
+                data = json.load(file)
     except Exception as e:
         print(e)
-        print("No cookies found")
-    return cookies
+        print("No data found")
+    return data
 
 
-def write_json_file(data):
-    with open(DATA_JSON_PATH, 'w') as file:
+def write_json_file(path, data):
+    with open(path, 'w') as file:
         json.dump(data, file)
     print('New configurations saved successfully')
 
 
 def sync_static_configuration():
     print("Syncing static configurations...")
-    current_dynamic_configurations = read_json_file()
+    current_dynamic_configurations = read_json_file(DATA_JSON_PATH)
 
     current_xpaths = current_dynamic_configurations.get("xpaths", {})
     current_link_labels = current_dynamic_configurations.get("link_labels", {})
@@ -42,7 +42,7 @@ def sync_static_configuration():
         if key not in current_link_labels:
             current_link_labels[key] = value
 
-    write_json_file({
+    write_json_file(DATA_JSON_PATH, {
         "xpaths": current_xpaths,
         "link_labels": current_link_labels
     })
