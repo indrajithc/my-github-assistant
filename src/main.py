5f0e1394c7cc0f2c1f4b9417d7feec1b78846ddf
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-05-17 22:58:16 +0530

    refactor: CACHE_PATH creation logic added

--- src/main.py
+++ src/main.py
@@ -4,10 +4,14 @@ from src.auth import login_github
 from src.utils.selenium_utils import get_selenium_driver
 from src.github_manager import main_action
 from dotenv import load_dotenv 
+from src.utils.constants import CACHE_PATH
 
 def main():
     # Load environment variables from .env file
     load_dotenv()
+    
+    if not os.path.exists(CACHE_PATH):
+        os.makedirs(CACHE_PATH)
     
     # Get Selenium driver
     driver = get_selenium_driver()
