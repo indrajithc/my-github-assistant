9cca11eecbb24057314b7d9b00b489ee08de7d87
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-05-17 16:40:34 +0530

    refactor: moved to different modules

--- src/main.py
+++ src/main.py
@@ -1,46 +1,18 @@
 import os
-import getpass
-import yaml
-from dotenv import load_dotenv
+import getpass  
 from src.auth import login_github
 from src.utils.selenium_utils import get_selenium_driver
-
-class Credentials:
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-def get_github_credentials():
-    """
-    Read GitHub username and password from config.yaml file.
-    If not found, prompt user for password.
-    """
-    username = os.getenv('GITHUB_USERNAME')
-    password = os.getenv('GITHUB_PASSWORD')
-    
-    if not username or not password:
-        print("GitHub username or password not found in environment variables.")
-
-    if not username:
-        username = input('Enter your GitHub username: ')
-
-    if not password:
-        password = getpass.getpass(prompt='Enter your GitHub password: ')
-    
-    return Credentials(username, password)
+from src.github_manager import main_action
+from dotenv import load_dotenv 
 
 def main():
     # Load environment variables from .env file
     load_dotenv()
-
-    # Get GitHub credentials
-    github_credentials = get_github_credentials()
     
     # Get Selenium driver
     driver = get_selenium_driver()
-    
-    # Login to GitHub
-    login_github(driver, github_credentials)
+
+    main_action(driver)
 
     # Add more functionality here
 
