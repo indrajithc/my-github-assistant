7638524749488e620dfb209a093c81345ae6e329
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-05-17 23:14:36 +0530

    feat: link_labels added

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -6,3 +6,7 @@ IMPLICIT_ELEMENT_WAIT_DELAY = 10
 SESSION_USER_XPATH = "//*[@aria-label='Open user account menu']"
 GITHUB_HOME_URL = "https://github.com"
 CACHE_PATH = "./.cache/"
+
+link_labels = {
+ "YOUR_PROFILE": "Your profile"
+}
