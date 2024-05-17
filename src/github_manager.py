dc985f5a30ad6ad4e42d7527c6e851071c48efe9
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-05-17 23:16:08 +0530

    feat: open_user_profile

--- src/github_manager.py
+++ src/github_manager.py
@@ -7,22 +7,21 @@ from src.utils.common_utils import wait_until
 from src.utils.constants import link_labels
 
 
-
 def open_user_account_menu(driver):
     """
     Open user account menu.
     """
     print("Opening user account menu...")
     driver.find_element(By.XPATH, SESSION_USER_XPATH).click()
-    
+
     def check_element():
-        if wait_until_element_present(driver, (By.XPATH,SESSION_USER_XPATH)):
+        if wait_until_element_present(driver, (By.XPATH, SESSION_USER_XPATH)):
             return True
         else:
             return False
 
     wait_until(check_element, 360)
-            
+
 
 def click_element_by_text(driver, text):
     """
@@ -31,7 +30,7 @@ def click_element_by_text(driver, text):
     print(f"Clicking element by text: {text}")
     element = driver.find_element(By.LINK_TEXT, text)
     element.click()
-    
+
     def check_element():
         if wait_until_element_present(driver, (By.LINK_TEXT, text)):
             return True
@@ -39,21 +38,26 @@ def click_element_by_text(driver, text):
             return False
 
     wait_until(check_element, 360)
-    
- 
+
+
+def open_user_profile(driver):
+    """
+    Open user profile.
+    """
+    print("Opening user profile...")
+
+    open_user_account_menu(driver)
+
+    click_element_by_text(driver, link_labels["YOUR_PROFILE"])
+
+
 def main_action(driver):
 
     # Get GitHub credentials
     github_credentials = get_github_credentials()
-    
+
     # Login to GitHub
     login_github(driver, github_credentials)
-    
-    # Open user account menu
-    open_user_account_menu(driver)
-    
-    click_element_by_text(driver, link_labels["YOUR_PROFILE"])
-    
-    
 
- 
+    # Open user profile
+    open_user_profile(driver)
