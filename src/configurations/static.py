10e8e74a7b90fc260d614bd51e8c0d32d2bc95e7
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-05-18 16:34:02 +0530

    refactor: paths added

--- src/configurations/static.py
+++ src/configurations/static.py
@@ -1,6 +1,9 @@
 link_labels = {
-    "YOUR_PROFILE": "Your profile"
+    "YOUR_PROFILE": "Your profile",
+    "FOLLOWERS": "followers",
+    "FOLLOWING": "following",
 }
 xpaths = {
-    "SESSION_USER_XPATH": "//*[@aria-label='Open user account menu']"
+    "SESSION_USER_XPATH": "//*[@aria-label='Open user account menu']",
+    "PERSON_XPATH": "//*[@itemtype='http://schema.org/Person']"
 }
