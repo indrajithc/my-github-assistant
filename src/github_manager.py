96dd6c3d322918b850ccea989f26a5d91c4891ae
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-05-18 18:34:08 +0530

    feat: get_followers_container added

--- src/github_manager.py
+++ src/github_manager.py
@@ -3,7 +3,7 @@ from src.utils.credentials import get_github_credentials
 from src.utils.common_utils import wait_until_element_present
 from selenium.webdriver.common.by import By
 from src.utils.common_utils import wait_until
-from src.configurations.read import read_label, read_xpath
+from src.configurations.read import read_label, read_xpath, read_id
 
 
 def open_user_account_menu(driver):
@@ -84,7 +84,7 @@ def load_all_followers(driver):
 
     if item_person:
         def check_element():
-            if wait_until_element_present(driver, (By.PARTIAL_LINK_TEXT, read_label("FOLLOWER"))):
+            if wait_until_element_present(item_person, (By.PARTIAL_LINK_TEXT, read_label("FOLLOWER"))):
                 return True
             else:
                 return False
@@ -100,6 +100,50 @@ def load_all_followers(driver):
             print("Element not found")
 
 
+def get_followers_container(driver):
+    """
+    Get followers container.
+    """
+    print("Getting followers container...")
+
+    def check_element():
+        if wait_until_element_present(driver, (By.ID, read_id("USER_PROFILE_FRAME"))):
+            return True
+        else:
+            return False
+
+    wait_until(check_element, 360)
+
+    element = driver.find_element(
+        By.ID, read_id("USER_PROFILE_FRAME"))
+    if element:
+        print("Followers container found")
+    else:
+        print("Followers container not found")
+        return None
+    return element
+
+
+def sync_followers_list(driver):
+    """
+    Sync followers list.
+    """
+    print("Syncing followers list...")
+
+    followers_container = get_followers_container(driver)
+
+    if followers_container:
+        item_container = followers_container.find_element(
+            By.XPATH, read_xpath("FOLDER_LIST_CONTAINER"))
+        if item_container:
+            print("Item container found")
+            for each_item in item_container:
+                print(each_item.text)
+        else:
+            print("Item container not found")
+            return None
+
+
 def main_action(driver):
 
     # Get GitHub credentials
@@ -112,3 +156,5 @@ def main_action(driver):
     open_user_profile(driver)
 
     load_all_followers(driver)
+
+    sync_followers_list(driver)
