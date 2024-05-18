659d0444ebefd0f4783e751b9c40948699796b00
Indrajith C <25261536+indrajithc@users.noreply.github.com>
2024-05-18 23:07:44 +0530

    refactor: selector updated

--- src/configurations/static.py
+++ src/configurations/static.py
@@ -6,7 +6,7 @@ link_labels = {
 xpaths = {
     "SESSION_USER_XPATH": "//*[@aria-label='Open user account menu']",
     "PERSON_XPATH": "//*[@itemtype='http://schema.org/Person']",
-    "FOLDER_LIST_CONTAINER": "//*[@data-hpc]",
+    "FOLDER_LIST_CONTAINER": "//div[@data-hpc]",
 }
 ids = {
     "USER_PROFILE_FRAME": "user-profile-frame"
